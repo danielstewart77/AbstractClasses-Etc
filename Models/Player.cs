bb43a443abb67d2e10f26ea30b99db6e0824f104
Daniel Stewart <daniel.stewart77@gmail.com>
2022-05-21 09:02:34 -0500

    created abstract class Player that implements IPlayer added some regular methods and an abstract method

--- Models/Player.cs
+++ Models/Player.cs
@@ -1,17 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Abstraction.Interfaces;
 
-namespace Virtual_Methods
+namespace Abstraction.Models
 {
 	/// <summary>
 	/// Base player model used for players & bots
 	/// </summary>
-	public class Player : IPlayer
+	public abstract class Player : IPlayer
 	{
-		public Guid Id => Guid.NewGuid();
+		private int _level;
+
+		private Guid _id;
+
+		public Player()
+		{
+			_id = Guid.NewGuid();
+			_level = 1;
+		}
+		public Guid Id { get { return _id; } set { _id = value; } }
 
 		/// <summary>
 		/// players can have empty names, bots should not
@@ -19,8 +25,20 @@ namespace Virtual_Methods
 		public virtual string Name { get; set; }
 
 		/// <summary>
-		/// regular players start at power 1
+		/// regular players start at power 1, bots are based on their level
 		/// </summary>
-		public virtual int Power { get; set; } = 1;
+		public virtual int Power { get; set; } = 10;
+
+		/// <summary>
+		/// All players start at level 1, bots level increases during the level
+		/// </summary>
+		public int Level { get { return _level; } set { _level = value; } }
+
+		public abstract void LevelUpDown(int up);
+
+		public void PowerUpDown(int pow)
+		{
+			Power += pow;
+		}
 	}
 }
