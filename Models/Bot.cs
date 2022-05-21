1e737544d77db5b9d481c263b7da5a24880fb545
Daniel Stewart <daniel.stewart77@gmail.com>
2022-05-21 09:04:43 -0500

    derived Infantry and Bot classes from the Player abstract class

--- Models/Bot.cs
+++ Models/Bot.cs
@@ -1,31 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Virtual_Methods
+﻿
+namespace Abstraction.Models
 {
 	/// <summary>
 	/// This class creates a Bot for a given level
 	/// </summary>
 	public class Bot : Player
 	{
-		private readonly int _level;
+		private readonly string _name;
+
 		/// <summary>
 		/// A Bot's power is based on it's level
 		/// </summary>
 		/// <param name="level"></param>
 		public Bot(int level) 
 		{ 
-			_level = level;
+			_name = $"Karen-{base.Id.ToString().Substring(0, 5)}";
+			Power = level;
 		}
 
 		/// <summary>
 		/// Bots are automatically given a name based on their Ids
 		/// </summary>
-		public override string Name { get => $"Karen-{base.Id.ToString().Substring(0, 5)}"; }
+		public override string Name { get => _name; }
 
-		public override int Power { get => _level; }
+		/// <summary>
+		/// Bots power is based on its level
+		/// </summary>
+		public override int Power { get; set; }
+
+		/// <summary>
+		/// Bots level up based on the level clock.
+		/// As the players spend more time in the level, the bots level => power increases
+		/// </summary>
+		/// <param name="timeOffest"></param>
+		public override void LevelUpDown(int timeOffest)
+		{
+			Level += timeOffest;
+		}
 	}
 }
---
+++ Models/Infantry.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Abstraction.Models
+{
+	/// <summary>
+	/// Lowest level human player character
+	/// </summary>
+	public class Infantry : Player
+	{
+		/// <summary>
+		/// For new player creation
+		/// </summary>
+		public Infantry() { }
+
+		/// <summary>
+		/// for loading players from a file
+		/// </summary>
+		/// <param name="level"></param>
+		/// <param name="id"></param>
+		/// <param name="name"></param>
+		/// <param name="power"></param>
+		public Infantry(int level, Guid id, string name, int power, string email)
+		{
+			Level = level;
+			Id = id;
+			Name = name;
+			Power = power;
+			Email = email;
+		}
+
+		public override void LevelUpDown(int up)
+		{
+			base.Power += up;
+		}
+
+		public string Email { get; set; }
+
+		public delegate void Action();
+
+		private Action _act;
+
+		/// <summary>
+		/// Here I'm implementing my delegate the
+		/// way Jonathan Gollnick. His was way more 
+		/// slick and gave me a better idea of how 
+		/// I could use them in my game
+		/// </summary>
+		public Action Act { set => _act = value; }
+
+		public void DoAction()
+		{
+			if (_act != null)
+			{
+				_act();
+			}
+		}
+	}
+}
